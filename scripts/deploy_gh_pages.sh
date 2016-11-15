da686a8a649440be49d1590aa377df1d813a0ab7
Holman Gao <holman@golmansax.com>
2016-11-14 19:43:26 -0500

    Try deploying to github pages on CI

---
+++ scripts/deploy_gh_pages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd public
+git init
+
+git config user.name "Holman Gao"
+git config user.email "holman@golmansax.com"
+
+git add .
+git commit -m 'Deploy to Github Pages'
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages
