ef73b41e043fc99c0e76c24f2489b67a1de016df
Holman Gao <holman@golmansax.com>
2016-11-17 23:04:01 -0500

    Appease lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-  extends: 'golmansax/rules/es6',
+  extends: 'golmansax',
   rules: {
     'arrow-body-style': 0,
+    'max-len': [2, 100],
+    'no-process-env': 1,
+    'no-underscore-dangle': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-filename-extension': [2, { extensions: [".js"] }],
   },
 };
