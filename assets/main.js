9e7ae328ef7cf7c8aac3212897fe1aba1ee33456
Holman Gao <holman@golmansax.com>
2016-11-23 18:20:35 -0500

    Add image zooming

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import ImageModal from '../lib/components/ImageModal';
+import './css/main.scss';
+
+render(<ImageModal />, document.getElementById('image-modal'));
